c8f946e9cc6f0bc141b5cedb8328aa7e902eb6c2
valeryvash <shestak.valery@gmail.com>
2021-10-21 20:43:33 +0300

    First practical task updated. Added demo file.

---
+++ src/main/java/com/valeryvash/module1_2_tasks/CommonElements/CommonElementsDemo.java
@@ -0,0 +1,22 @@
+package main.java.com.valeryvash.module1_2_tasks.CommonElements;
+
+import java.util.ArrayList;
+import java.util.Arrays;
+import java.util.List;
+
+import static main.java.com.valeryvash.module1_2_tasks.CommonElements.CommonElementsCollection.getCommonElements;
+
+public class CommonElementsDemo {
+
+    public static void main(String[] args) {
+
+        Integer[] fArray = {6, 4, 6, 8, 1, 100, -100};
+        Integer[] sArray = {3,8,76544, -42, 100,1};
+
+        System.out.println(getCommonElements(
+                new ArrayList<>(Arrays.asList(fArray)),
+                new ArrayList<>(Arrays.asList(sArray))
+        ));
+
+    }
+}
