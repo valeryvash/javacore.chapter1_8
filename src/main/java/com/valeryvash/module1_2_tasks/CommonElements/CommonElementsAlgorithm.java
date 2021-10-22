dd7b67bd65746596478a4dd0840d1dec48063146
valeryvash <shestak.valery@gmail.com>
2021-10-22 17:19:37 +0300

    Massive based algorithm version of first task added.

--- src/main/java/com/valeryvash/module1_2_tasks/CommonElements/CommonElementsAlgorithm.java
+++ src/main/java/com/valeryvash/module1_2_tasks/CommonElements/CommonElementsAlgorithm.java
@@ -1,143 +1,53 @@
 package main.java.com.valeryvash.module1_2_tasks.CommonElements;
 
 
+import java.util.Arrays;
 
 public class CommonElementsAlgorithm {
-    /*
-    I need to realize algorithm which returns intersection collection.
-    Input data is two collections of integers.
-    Example:
-    [6,4,6,8,1,100,-100], [3,8,76544, -42, 100,1]
-    Result:
-    [1,8,100]
-     */
-    public static void main(String[] args) {
 
-        int[] firstCollection = {6,4,6,8,1,100,-100};
-        int[] secondCollection = {3,8,76544, -42, 100,1};
+    public static void insertionSort(int[] massive) {
 
-        // Intersected collection will not be greater that minimal collection
-        int[] intersectionCollection = new int[
-                Math.min(firstCollection.length, secondCollection.length)
-                ];
-        // If one collection have zero length we shall interrupt program work
-        if (intersectionCollection.length == 0) {
-            System.out.println(
-                    "One collection has no elements."
-            );
-            throw new RuntimeException();
-        }
+        int length = massive.length;
 
-        // Does collections have intersection?
-        // I need min and max elements of both collections
-        // First I need to init int values with collections first elements
-
-        int
-                min_1st = firstCollection[0],
-                max_1st = min_1st,
-                min_2nd = secondCollection[0],
-                max_2nd = min_2nd;
-
-        for (int j : firstCollection) {
-            if (j < min_1st) min_1st = j;
-            if (j > max_1st) max_1st = j;
-        }
-
-        for (int j : secondCollection) {
-            if (j < min_2nd) min_2nd = j;
-            if (j > max_2nd) max_2nd = j;
-        }
-
-        /*
-            Check intersection condition
-         */
-        if (max_1st < min_2nd)
-        {
-            System.out.println("1st collection located to the left of 2nd. " +
-                    "Collection have no intersection");
-            new RuntimeException();
-        }
-        if (max_2nd < min_1st)
-        {
-            System.out.println("1st collection located to the right of 2nd. " +
-                    "Collection have no intersection");
-            new RuntimeException();
-        }
-
-        System.out.println("Collections might have common elements");
-
-        // Here I use bool massive as
-        // Duplicated values also will be included in final intersection
-
-        boolean check[] = new boolean[Math.max(
-                        firstCollection.length, secondCollection.length)];
-
-        //Init counter. It is needed for precise array init
-        int counter = 0;
-
-        if (firstCollection.length >= secondCollection.length)
-        {
-            for (int first_element:
-                 firstCollection) {
-                for (int i = 0; i < secondCollection.length; i++) {
-                    if (first_element == secondCollection[i] &&
-                            !check[i]) {
-                        check[i] = true;
-                        counter++;
-                    }
-                }
+        for (int i = 1; i < length ; i++) {
+            int temp = massive[i];
+            int j = i - 1;
+            for (; j >= 0 && temp < massive[j]; j--)
+            {
+                massive[j + 1] = massive[j];
             }
-        } else {
-            for (int first_element:
-                    secondCollection) {
-                for (int i = 0; i < firstCollection.length; i++) {
-                    if (first_element == firstCollection[i] &&
-                            !check[i]) {
-                        check[i] = true;
-                        counter++;
-                    }
-                }
-            }
+            massive[j + 1] = temp;
         }
-
-        intersectionCollection = new int[counter];
-
-        //TODO: complete bad algorithm version
-        if (firstCollection.length >= secondCollection.length){
-            for (int i = 0; i < secondCollection.length; i++) {
-                if (check[i])
-                {
-                    intersectionCollection[counter---1]=secondCollection[i];
-                }
-            }
-        } else {
-            for (int i = 0; i < firstCollection.length; i++) {
-                if (check[i])
-                {
-                    intersectionCollection[counter---1]=firstCollection[i];
-                }
-            }
-        }
-
-        // Print intersection massive in console
-
-        for (int v:
-             intersectionCollection) {
-            System.out.print(v + "  ");
-        }
-        System.out.println();
-
-
-
-
-
-
-
-
-
-
-
-
     }
 
+    public static int[] getMassiveOfCommonsElements(int[] first, int[] second)
+    {
+        boolean[] checked = new boolean[second.length];
+
+        Arrays.fill(checked, false);
+
+        int count = 0;
+
+        for (int i = 0; i < first.length; i++)
+        {
+                for (int j = 0; j < second.length; j++)
+                {
+                    if ((first[i] == second[j]) && (!checked[j]))
+                    {
+                        checked[j]    = true;
+                        count++;
+                        break;
+                    }
+                }
+        }
+
+        int[] ret_val = new int[count];
+        for (int i = 0; i < checked.length; i++) {
+            if (checked[i]) ret_val[--count] = second[i];
+        }
+
+        insertionSort(ret_val);
+
+        return ret_val;
+    }
 }
--- src/main/java/com/valeryvash/module1_2_tasks/CommonElements/CommonElementsDemo.java
+++ src/main/java/com/valeryvash/module1_2_tasks/CommonElements/CommonElementsDemo.java
@@ -3,18 +3,27 @@ package main.java.com.valeryvash.module1_2_tasks.CommonElements;
 import java.util.ArrayList;
 import java.util.Arrays;
 
-import static main.java.com.valeryvash.module1_2_tasks.CommonElements.CommonElementsCollection.getCommonElements;
+import static main.java.com.valeryvash.module1_2_tasks.CommonElements.CommonElementsCollection.*;
+import static main.java.com.valeryvash.module1_2_tasks.CommonElements.CommonElementsAlgorithm.*;
 
 public class CommonElementsDemo {
 
     public static void main(String[] args) {
 
-        Integer[] fArray = {6, 4, 6, 8, 1, 100, -100};
-        Integer[] sArray = {3,8,76544, -42, 100,1};
+//        Integer[] fArray = {6, 4, 6, 8, 1, 100, -100};
+//        Integer[] sArray = {3,8,76544, -42, 100,1};
+//
+//        System.out.println(getCommonElements(
+//               new ArrayList<>(Arrays.asList(fArray)),
+//                new ArrayList<>(Arrays.asList(sArray))
+//       ));
 
-        System.out.println(getCommonElements(
-                new ArrayList<>(Arrays.asList(fArray)),
-                new ArrayList<>(Arrays.asList(sArray))
-        ));
+        int[] left =  {6, 4, 6,-15,9, -8, 1, 100, -100};
+        int[] right = {9,-15,3,8,76544, -42, 100,1,-100};
+
+        int[] array = getMassiveOfCommonsElements(left, right);
+
+        System.out.println(Arrays.toString(array));
+
     }
 }
