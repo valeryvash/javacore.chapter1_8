fc95a97155a7847523e92a1f1e722e8c9297d77d
valeryvash <shestak.valery@gmail.com>
2021-10-25 13:58:50 +0300

    TreeMapDemo2A added. Complex (sequential) comparator usage.

---
+++ src/main/java/com/valeryvash/javacore/chapter18/TreeMapDemo2A.java
@@ -0,0 +1,66 @@
+package main.java.com.valeryvash.javacore.chapter18;
+
+import java.util.Comparator;
+import java.util.Map;
+import java.util.Set;
+import java.util.TreeMap;
+
+class CompLastNames implements Comparator<String> {
+    @Override
+    public int compare(String o1, String o2) {
+        int i, j;
+
+        i = o1.lastIndexOf(' ');
+        j = o2.lastIndexOf(' ');
+        return o1.substring(i)
+                .compareToIgnoreCase(
+                        o2.substring(j)
+                );
+    }
+}
+
+class CompThenByFirstName implements Comparator<String> {
+
+    @Override
+    public int compare(String o1, String o2) {
+        int i, j;
+
+        return o1
+                .compareToIgnoreCase(
+                        o2
+                );
+    }
+
+}
+
+public class TreeMapDemo2A {
+    public static void main(String[] args) {
+        CompLastNames compLastNames = new CompLastNames();
+        Comparator<String> compLastThenFirst =
+                compLastNames.thenComparing(new CompThenByFirstName());
+
+        TreeMap<String, Double> tm = new TreeMap<>(compLastThenFirst);
+
+        tm.put("John Dow", 3434.12);
+        tm.put("Tom Smith", 123.12);
+        tm.put("Jane Baker", 1378.00);
+        tm.put("Tod Hall", 99.22);
+        tm.put("Ralf Smith", -19.08);
+
+        Set<Map.Entry<String, Double>> set = tm.entrySet();
+
+        for (Map.Entry<String, Double> temp : set) {
+            System.out.print(temp.getKey() + " : ");
+            System.out.println(temp.getValue());
+        }
+
+
+        String user = "John Dow";
+        double balance = tm.get(user);
+        tm.put(user, balance + 1000);
+
+        System.out.print("New balance of " + user + ": ");
+
+        System.out.println(tm.get(user));
+    }
+}
